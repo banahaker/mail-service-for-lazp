e2b096c008b5ee7317a3b7dd384f6c0523215e01
banahaker <pn0818x@outlook.com>
2022-07-17 13:26:56 +0800

    feat: email epi

--- pages/send/index.tsx
+++ pages/send/index.tsx
@@ -1,10 +1,34 @@
+import axios from "axios";
 import type { NextPage } from "next";
 import { useState } from "react";
+import apis from "../../apis";
 import styles from "../../styles/Send.module.css";
 
 const Send: NextPage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [targetEmail, setTargetEmail] = useState("");
+  const sendEmail = (title: string, content: string, email: string) => {
+    if (title == "" || content == "" || email == "") return;
+    const { TOKEN } = localStorage;
+    axios
+      .post(apis.Host + apis.Mail + `/${email}`, { title, content, TOKEN })
+      .then((res) => {
+        const data = res.data;
+        if (data.status == "Succeed") {
+          alert("Send Email Succeed");
+          setTitle("");
+          setTargetEmail("");
+          setContent("");
+        } else {
+          alert("Send Email Fail:");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Server Fail");
+      });
+  };
   return (
     <div className={styles.wrap}>
       <div className={styles.Send}>
@@ -14,12 +38,23 @@ const Send: NextPage = () => {
           placeholder="Title"
           onChange={(e) => setTitle(e.target.value)}
         />
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setTargetEmail(e.target.value)}
+        />
         <textarea
           name="content"
           id="content"
           onChange={(e) => setContent(e.target.value)}
         ></textarea>
-        <button>Send</button>
+        <button
+          onClick={() => {
+            sendEmail(title, content, targetEmail);
+          }}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
